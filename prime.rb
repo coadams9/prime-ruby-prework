b6371b306ba50aba13bffbbb4e1f3b8e1315f110
Cory Adams <coadams9@gmail.com>
2018-10-26 20:42:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 def prime?(num)
-  for x in (2..num)
-   if (num % x) != 0
+  # if the number is less than or equal to one it's not prime
+  # if number is divisible by only itself and 1 a whole number of times it is prime
+  # if number is divisible by any number less than it a whole numberof times it is not prime
+  if num <= 1
     return false
-    else true
+  end
+  for x in (2...num)
+   if (num % x) == 0
+    return false
    end
   end
-
+  return true
 end
